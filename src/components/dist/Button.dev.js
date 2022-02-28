6be2ec1710fd9ca608f29d84d8d99e971df37cc2
devanshi-code <devu2732@gmail.com>
2022-02-28 17:40:08 +0530

    commit

---
+++ src/components/dist/Button.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+require("./Button.css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+//# sourceMappingURL=Button.dev.js.map
